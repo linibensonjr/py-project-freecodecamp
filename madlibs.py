57753ceb326fa61140ed9253f031caa51c5b0389
Iniobong Benson <linibensonjr@gmail.com>
2022-05-09 21:16:12 +0100

    Complete Mad Libs Game

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+name = input('What\'s your name: ')
+gender = input('What\'s your gender: ')
+occupation = input('What\'s your occupantion (school or work): ')
+hobby = input('What do you do on your way to '+ occupation+' ')
+
+
+
+
+print("My name is {} and I am a {}, I love {} when I compute to {}, once, when I was\
+    on my way to {}, I met John who would become my bestfriend".format(name, gender, hobby, occupation, occupation))
